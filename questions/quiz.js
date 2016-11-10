7c9c92a6e7db725f2f856e35a6b153190c473012
Julian Akkaya <notme@example.aq>
2016-11-10 21:15:17 +0100

    text input questions added

--- questions/quiz.js
+++ questions/quiz.js
@@ -5,7 +5,8 @@ var questions = [
 		answer2 = "answer2",
 		answer3 = "answer3",
 		answer4 = "deshalb deshalb",
-		rightAnswer ="3"
+		rightAnswer ="3",
+		category = "radio"
 	],
 	question2 = [
 		answer1 = "deshalb",
@@ -13,40 +14,61 @@ var questions = [
 		answer3 = "answer3",
 		answer4 = "answer4",
 		question = "foo Bar?",
-		rightAnswer ="0"
+		rightAnswer ="0",
+		category = "radio"
+	],
+	question3 = [
+		answer1 = "",
+		answer2 = "",
+		answer3 = "",
+		answer4 = "",
+		question = "Wie heißt die 1. Schicht des OSI Models?",
+		rightAnswer = "Bituebertragungsschicht",
+		category = "text"
 	]
 ];
 
+//Program start
+const notAnswersInQuestionArray = 3;
 var questionId = 0;
+
+function inputQuestion (questionId) {
+	var answers = "";
+	document.getElementById("question").innerHTML = questions[questionId][4];
+	if (questions[questionId][6] == "radio"){
+		for (i = 0; i < questions[questionId].length - notAnswersInQuestionArray; i++) {
+			answers += "<button type='button' onclick='controler("+i+")''>"+questions[questionId][i]+"</button><br />";
+		}
+		document.getElementById("answers").innerHTML = answers;
+	} else {
+		document.getElementById("answers").innerHTML = "<input type='input' id='inputText'><br /><button type='button' onclick='controler(\"textAnswer\")'>Antworten</button>";
+	}
+}
+
+function randomQuestion() {
+	var questionId = Math.floor(Math.random() * questions.length)
+	return questionId;
+}
 
 function controlAnswer(userAnswer, questionId, questions) {
 	console.log("User Answer:" + userAnswer);
+	console.log("Control answer, for question:" + questionId);
+	console.log("The right answer is:" + questions[questionId][5]);
 	if(userAnswer == questions[questionId][5]) {
 		console.log("The answer was: true");
 		return true;
 	} else {
 		console.log("The answer was: false");
 		return false;
-	}	
-}
-
-function randomQuestion() {
-	var questionId = Math.floor(Math.random() * questions.length)
-	console.log("Anzahl an Fragen: " + questions.length);
-	return questionId;
-}
-
-function inputQuestion (questionId) {
-	answers = "";
-	for (i = 0; i < questions[questionId].length - 2; i++) {
-		answers += "<button type='button' onclick='changeQuestion("+i+")''>"+questions[questionId][i]+"</button><br />";
 	}
-	document.getElementById("question").innerHTML = questions[questionId][4];
-	document.getElementById("answers").innerHTML = answers;
 }
 
-function changeQuestion(userAnswer) {
-	if (!controlAnswer(userAnswer, questionId, questions)) {
+function controler(userAnswer) {
+	if(userAnswer == "textAnswer") {
+		userAnswer = document.getElementById("inputText").value;
+	}
+	if (!controlAnswer(userAnswer, questionId, questions)){
+		console.log("Wrong answer. Try it again!");
 		return "";
 	}
 	inputQuestion(questionId = randomQuestion());
